8830c1c6a0a6aaf6cc775d5fbdf77854f5159e99
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-05-18 17:43:57 +0800

    Confoming with pep8 requirements

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,19 @@
-from flask import render_template
+from flask import render_template, jsonify
 from app import app, db
 from app.forms import Login, SignUp, CitizenReport, CitizenStatus
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import Citizen, Report, Status
+from app.models import Citizen, Report, Status, Image
 from flask import redirect, url_for, flash, request
 from sqlalchemy import func
-import string, random
+import string
+import random
+from requests import get
+
 
 def gen_id():
     chars = string.digits
     return int(''.join(random.choice(chars) for _ in range(0, 6)))
+
 
 def get_links():
     return [
@@ -27,10 +31,12 @@ def get_links():
         }
     ]
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Welcome', links=get_links())
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -39,7 +45,11 @@ def register():
     form = SignUp()
     if form.validate_on_submit():
         try:
-            citizen = Citizen(citizen_id=form.citizen_id.data, name=form.citizen_id.data, score=0)  
+            citizen = Citizen(
+                citizen_id=form.citizen_id.data,
+                name=form.citizen_id.data,
+                score=0
+            )
             citizen.set_password(form.password.data)
             db.session.add(citizen)
             db.session.commit()
@@ -47,7 +57,9 @@ def register():
             return redirect(url_for('feed'))
         except Exception as e:
             print('There was an error creating new user.' + str(e))
-    return render_template('register.html', links=get_links(), title='Register', form=form)
+    return render_template(
+        'register.html', links=get_links(), title='Register', form=form)
+
 
 @app.route('/feed', methods=['GET', 'POST'])
 @login_required
@@ -59,7 +71,8 @@ def feed():
     form = CitizenReport(reporter=current_user.citizen_id)
     status_input = CitizenStatus()
     if form.validate_on_submit():
-        reported = Citizen.query.filter_by(citizen_id=form.traitor.data).first()
+        reported = Citizen.query.filter_by(
+            citizen_id=form.traitor.data).first()
         if reported is None:
             invalid_citizen = True
             return redirect(url_for('feed'))
@@ -67,7 +80,11 @@ def feed():
             try:
                 reported.score = reported.score + float(form.category.data)
                 db.session.commit()
-                new_report = Report(reporter_id=current_user.citizen_id, reported_id=reported.citizen_id, report_id=gen_id(), body=form.body.data)
+                new_report = Report(
+                    reporter_id=current_user.citizen_id,
+                    reported_id=reported.citizen_id,
+                    report_id=gen_id(),
+                    body=form.body.data)
                 db.session.add(new_report)
                 db.session.commit()
                 print('Successful report submission')
@@ -78,10 +95,15 @@ def feed():
         print(form.errors)
     if status_input.validate_on_submit():
         try:
-            current_citizen = Citizen.query.filter_by(citizen_id=current_user.citizen_id).first()
-            current_citizen.score = current_citizen.score + float(status_input.status_category.data)
+            current_citizen = Citizen.query.filter_by(
+                citizen_id=current_user.citizen_id).first()
+            current_citizen.score = current_citizen.score + float(
+                status_input.status_category.data)
             db.session.commit()
-            new_status = Status(citizen_id=current_user.citizen_id, status_id=gen_id(), body=status_input.status.data)
+            new_status = Status(
+                citizen_id=current_user.citizen_id,
+                status_id=gen_id(),
+                body=status_input.status.data)
             db.session.add(new_status)
             db.session.commit()
             print('Status submission successful')
@@ -90,41 +112,77 @@ def feed():
     else:
         print('Status form validation error')
         print(status_input.errors)
-    reports = Report.query.order_by(Report.time.desc()).paginate(reports_page, 5, False)
+    reports = Report.query.order_by(
+        Report.time.desc()).paginate(reports_page, 5, False)
     next_reports = url_for('feed', reports=reports.next_num) \
         if reports.has_next else None
     prev_reports = url_for('feed', reports=reports.prev_num) \
         if reports.has_prev else None
-    all_status = Status.query.order_by(Status.timestamp.desc()).paginate(status_page, 5, False)
+    all_status = Status.query.order_by(
+        Status.timestamp.desc()).paginate(status_page, 5, False)
     next_statuses = url_for('feed', status=all_status.next_num) \
         if all_status.has_next else None
     prev_statuses = url_for('feed', status=all_status.prev_num) \
         if all_status.prev else None
-    return render_template('feed.html', title='Feed', form=form, reports=reports.items, status_input=status_input, 
-        statuses=all_status.items, next_reports=next_reports, prev_reports=prev_reports, next_status=next_statuses, prev_status=prev_statuses)
+    return render_template(
+        'feed.html',
+        title='Feed',
+        form=form,
+        reports=reports.items,
+        status_input=status_input,
+        statuses=all_status.items,
+        next_reports=next_reports,
+        prev_reports=prev_reports,
+        next_status=next_statuses,
+        prev_status=prev_statuses
+    )
+
 
 @app.route('/profile')
 @login_required
 def profile_home():
-    user_id = current_user.citizen_id;
+    user_id = current_user.citizen_id
     return redirect(url_for('profile', citizen_id=user_id))
+
 
 @app.route('/profile/<citizen_id>')
 @login_required
 def profile(citizen_id):
     citizen = Citizen.query.filter_by(citizen_id=citizen_id).first_or_404()
-    return render_template('profile.html', title='My Profile', links=get_links())
+    return render_template(
+        'profile.html', title='My Profile', links=get_links(), citizen=citizen)
+
+
+def get_images(num):
+    client_id = app.config['IMG_ACCESS']
+    orientation = 'squarish'
+    count = num
+    url = 'https://api.unsplash.com/photos/random/?client_id={}&orientation={}&count={}'.format(client_id, orientation, count)  # nopep8
+    content = get(url).json()
+    for img in content:
+        image = Image(image_url=img['urls']['small'])
+        db.session.add(image)
+    db.session.commit()
+
 
 @app.route('/rank')
 def rank():
     page = request.args.get('page', 1, type=int)
-    citizens = Citizen.query.order_by(Citizen.score.desc()).paginate(page, 10, False)
+    citizens = Citizen.query.order_by(
+        Citizen.score.desc()).paginate(page, 10, False)
     next_citizens = url_for('rank', page=citizens.next_num) \
         if citizens.has_next else None
     prev_citizens = url_for('rank', page=citizens.prev_num) \
         if citizens.has_prev else None
-    top_citizens = Citizen.query.filter(Citizen.score >= 5000).order_by(func.random()).limit(3)
-    return render_template('rank.html', citizens=citizens.items, tops=top_citizens, next=next_citizens, prev=prev_citizens)
+    top_citizens = Citizen.query.filter(
+        Citizen.score >= 5000).order_by(func.random()).limit(3)
+    return render_template(
+        'rank.html',
+        citizens=citizens.items,
+        tops=top_citizens,
+        next=next_citizens,
+        prev=prev_citizens)
+
 
 @app.route('/login',  methods=['GET', 'POST'])
 def login():
@@ -134,20 +192,24 @@ def login():
     if not form.validate_on_submit():
         print('Form did not validate ')
     if form.validate_on_submit():
-        citizen = Citizen.query.filter_by(citizen_id=form.citizen_id.data).first()
+        citizen = Citizen.query.filter_by(
+            citizen_id=form.citizen_id.data).first()
         if citizen is None or not citizen.check_password(form.password.data):
             print('Bad login attempt')
             return redirect(url_for('login'))
         login_user(citizen)
         flash('Good login')
         return redirect(url_for('feed'))
-    return render_template('login.html', form=form, links=get_links(), title="Login")
+    return render_template(
+        'login.html', form=form, links=get_links(), title="Login")
+
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
 
 @app.route('/about')
 def about():
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'wowza-super-secret'
 
