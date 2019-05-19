b48daebdfd4884476f9599dc0a49cbbfbb37bab3
Drew Alexander <22423862@student.uwa.edu.au>
2019-05-19 19:46:20 +0800

    Ekin did a Whoospie

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'wowza-super-secret'
 
     IMG_ACCESS = os.environ.get('IMAGE_ACCESS_KEY')
-    IMG_SECRET = os.environ.get('IMsAGE_SECRET_KEY')
+    IMG_SECRET = os.environ.get('IMAGE_SECRET_KEY')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
