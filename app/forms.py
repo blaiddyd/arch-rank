883d71b652e5d86c9f67aa2f7798aab12e0afee5
DrewAlexander98 <22423862@student.uwa.edu.au>
2019-05-20 04:36:04 +0800

    Big tings

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField  # nopep8
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, TextAreaField, DateField, IntegerField  # nopep8
 from wtforms.validators import DataRequired, EqualTo, ValidationError, Length
 from wtforms.fields.html5 import EmailField
 from app.models import Citizen
@@ -14,6 +14,22 @@ activities = [
     ('5000', 'Graduating from Divine Academy'),
     ('700', 'Praising the Supreme Commander'),
     ('900', 'Contributing to the Community')
+]
+professions = [
+    ('', 'Select your profession'),
+    (1, 'High Priest'),
+    (1, 'Royal Accountant'),
+    (1, 'Industrial Overseer'),
+    (1, 'High Court Scientist'),
+    (1, 'Counsel Advisor'),
+    (1, 'Traitor Hunter'),
+    (1, 'Mind Seer'),
+    (1, 'Royal Webmaster'),
+    (2, 'Personel Manager'),
+    (2, 'Comissioned Artist'),
+    (2, 'Ferryman'),
+    (2, 'Archival Custodian'),
+    (2, 'Public Lawyer')
 ]
 
 
@@ -48,7 +64,7 @@ class SignUp(FlaskForm):
     def validate_id(self, citizen_id):
         citizen = Citizen.query.filter_by(citizen_id=citizen_id.data).first()
         if citizen is not None:
-            raise ValidationError('This citizen already exists in Arch')
+            raise ValidationError('This citizen is already registered')
 
 
 class CitizenReport(FlaskForm):
@@ -59,7 +75,8 @@ class CitizenReport(FlaskForm):
     category = SelectField(
         'Type of Treason',
         render_kw={"placeholder": "Select an offense"},
-        choices=offenses, validators=[DataRequired()])
+        choices=offenses,
+        validators=[DataRequired()])
     body = TextAreaField(
         'More about your report',
         render_kw={"placeholder": "What would you like to report?"})
@@ -75,3 +92,66 @@ class CitizenStatus(FlaskForm):
         choices=activities,
         validators=[DataRequired()])
     status_submit = SubmitField('Submit Status')
+
+
+class Eval(FlaskForm):
+    full_name = StringField(
+        "Full Name",
+        render_kw={
+            "placeholder": "Full Name",
+            "data-validation": "custom",
+            "data-validation-regexp": "^([a-zA-Z]+)( ([a-zA-Z]+))+$",
+            "data-validation-error-msg": " "},
+        validators=[DataRequired()])
+    birth_date = DateField(
+        'DOB',
+        format='%d-%m-%Y',
+        render_kw={
+            "type": "date",
+            "data-validation": "date",
+            "data-validation-error-msg": " "},
+        validators=[DataRequired()])
+    home_address = StringField(
+        'Home Address',
+        render_kw={
+            "placeholder": "45 Highcourt Pl, Izmir",
+            "data-validation": "custom",
+            "data-validation-regexp": "^([a-zA-Z1-9]+)( ([a-zA-Z]+))+,( ([a-zA-Z]+))+$",  # nopep8
+            "data-validation-error-msg": " "},
+        validators=[DataRequired()])
+    profession = SelectField(
+        'Profession',
+        render_kw={
+            "data-validation": "required",
+            "data-validation-error-msg": " "},
+        choices=professions,
+        validators=[DataRequired()])
+    income = IntegerField(
+        'Income',
+        render_kw={
+            "type": "number",
+            "data-validation": "custom",
+            "data-validation-regexp": "^([1-9]+)$",
+            "step": "1000",
+            "data-validation-error-msg": " "},
+        validators=[DataRequired()])
+    married = BooleanField('Are you married?')
+    kids = SelectField(
+        'How many kids do you have?',
+        choices=[
+            (0, 'I have no children'),
+            (1, 'I have but one child'),
+            (2, 'I have two lovely children'),
+            (3, 'There are three children in my household'),
+            (4, 'Four children have graced my life with their smiles'),
+            (5, 'Not four but five! Five children are there to greet me when I wake'),  # nopep8
+            (6, 'The number of children I have is more than five but less than seven alas'),  # nopep8
+            (7, 'I have one child for each day of the beautiful week and I have named them accordingly'),  # nopep8
+            (8, 'My children are bountiful as the apples on the trees, as the islands of Agea, as the eight wise elders'),  # nopep8
+            (9, 'I have nine children')],
+        validators=[DataRequired()]
+    )
+    lonely = BooleanField(
+        'Are you lonely?',
+        default=True)
+    eval_submit = SubmitField('Submit for evaluation')
